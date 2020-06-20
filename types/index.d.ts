bd0623dafd88913f3c490dc237aae10949f74db2
Noel De Martin <noeldemartin@gmail.com>
2020-06-20 12:10:35 +0200

    Export EngineAttributeUpdateOperation type

--- types/index.d.ts
+++ types/index.d.ts
@@ -16,6 +16,7 @@ export {
     EngineAttributeFilter,
     EngineAttributeLeafValue,
     EngineAttributeUpdate,
+    EngineAttributeUpdateOperation,
     EngineAttributeValue,
     EngineDocument,
     EngineDocumentsCollection,
