a0fe6fefadd00442693ccec5fde91123629dc73d
Noel De Martin <noeldemartin@gmail.com>
2021-05-16 12:09:54 +0200

    Track booted Models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,8 @@
+import { fail } from '@noeldemartin/utils';
+
+import { SoukaiError } from '@/errors';
 import type { Model } from '@/models/Model';
+import type { ModelConstructor } from '@/models/inference';
 
 export * from './inference';
 export * from './Model';
@@ -26,8 +30,17 @@ export {
 
 export type { Attributes, AttributeValue } from './attributes';
 
+const bootedModels: Map<string, typeof Model> = new Map;
+
+export function requireBootedModel<T extends ModelConstructor<Model> = ModelConstructor<Model>>(name: string): T {
+    return bootedModels.get(name) as T
+        ?? fail(SoukaiError, `Could not find Model with name '${name}', are you sure it's booted?`);
+}
+
 export function bootModels(models: Record<string, typeof Model>): void {
     for (const [modelName, modelClass] of Object.entries(models)) {
         modelClass.boot(modelName);
+
+        bootedModels.set(modelName, modelClass);
     }
 }
