35a178d1fd09b8e25bc97b55acfc807e362f11a7
Noel De Martin <noeldemartin@gmail.com>
2021-09-18 09:01:23 +0200

    Refactor ModelKey to improve testing equality

--- src/models/ModelKey.ts
+++ src/models/ModelKey.ts
@@ -1,1 +1,17 @@
-export default class ModelKey extends String {}
+export default class ModelKey {
+
+    private value: string;
+
+    constructor(rawValue: unknown) {
+        this.value = String(rawValue);
+    }
+
+    public equals(other: ModelKey): boolean {
+        return this.value === other.value;
+    }
+
+    public toString(): string {
+        return this.value;
+    }
+
+}
