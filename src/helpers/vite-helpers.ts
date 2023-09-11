1eaac284ec6bc2e49cadae357a515d8f30740656
Noel De Martin <noel@noeldemartin.com>
2023-09-11 11:03:30 +0200

    Improve vite helpers

--- src/helpers/vite-helpers.ts
+++ src/helpers/vite-helpers.ts
@@ -1,5 +1,21 @@
 import { bootModels } from '@/models';
-import type { Model } from '@/models';
+import { Model } from '@/models/Model';
+
+function isModelClass(module: unknown): module is typeof Model {
+    let prototype = Object.getPrototypeOf(module);
+
+    while (prototype) {
+        if (prototype !== Model) {
+            prototype = Object.getPrototypeOf(prototype);
+
+            continue;
+        }
+
+        return true;
+    }
+
+    return false;
+}
 
 export function getModelsFromViteGlob(
     glob: Record<string, Record<string, unknown>>,
@@ -10,14 +26,21 @@ export function getModelsFromViteGlob(
     for (const [fileName, moduleExports] of Object.entries(glob)) {
         const name = (fileName.match(/^(.*\/)?(.+)\.(j|t)s$/) || [])[2];
 
-        if (typeof name === 'undefined')
+        if (typeof name === 'undefined') {
             continue;
+        }
 
-        if (ignoreSuffixes.some(suffix => name.endsWith(suffix)))
+        if (ignoreSuffixes.some((suffix) => name.endsWith(suffix))) {
             continue;
+        }
 
-        models[name] = (moduleExports['default'] ??
-            moduleExports[Object.keys(moduleExports)[0] as string]) as typeof Model;
+        const module = moduleExports['default'] ?? moduleExports[Object.keys(moduleExports)[0] as string];
+
+        if (!module || !isModelClass(module)) {
+            continue;
+        }
+
+        models[name] = module;
     }
 
     return models;
