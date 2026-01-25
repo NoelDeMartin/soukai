e980e208e9b27ec276053c0739e7a85caaf52206
Noel De Martin <noel@noeldemartin.com>
2026-01-25 18:22:40 +0100

    Implement finding containers

--- packages/soukai-bis/src/models/TypeIndex.ts
+++ packages/soukai-bis/src/models/TypeIndex.ts
@@ -1,3 +1,32 @@
-import Model from './TypeIndex.schema';
+import { arrayEquals, asyncFirst } from '@noeldemartin/utils';
 
-export default class TypeIndex extends Model {}
+import Model from './TypeIndex.schema';
+import Container from './Container';
+import type { ContainerConstructor, ModelConstructor } from './types';
+
+export default class TypeIndex extends Model {
+
+    public async findContainer<T extends Container = Container>(
+        modelClass: ModelConstructor,
+        containerClass?: ContainerConstructor<T>,
+    ): Promise<T | null> {
+        await this.loadRelationIfUnloaded('registrations');
+
+        const containerRegistrations =
+            this.registrations?.filter((registration) => {
+                return (
+                    registration.instanceContainer &&
+                    arrayEquals(
+                        registration.forClass,
+                        modelClass.schema.rdfClasses.map((rdfClass) => rdfClass.value),
+                    )
+                );
+            }) ?? [];
+
+        return asyncFirst(containerRegistrations, async (registration) =>
+            registration?.instanceContainer
+                ? ((containerClass ?? Container).find(registration.instanceContainer) as Promise<T>)
+                : null);
+    }
+
+}
