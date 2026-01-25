6c3ecce8ff340d1b8f78248bbce3de43bc0bfe77
Noel De Martin <noel@noeldemartin.com>
2026-01-25 19:25:43 +0100

    Implement Vite helpers

--- packages/soukai-bis/src/models/registry.ts
+++ packages/soukai-bis/src/models/registry.ts
@@ -2,12 +2,42 @@ import { fail } from '@noeldemartin/utils';
 
 import SoukaiError from 'soukai-bis/errors/SoukaiError';
 
+import { isModelClass } from './utils';
 import type Model from './Model';
 import type { ModelConstructor } from './types';
 
 const bootedModels: Map<string, ModelConstructor> = new Map();
 
 export interface ModelsRegistry {}
+
+export function getModelsFromViteGlob(
+    glob: Record<string, Record<string, unknown>>,
+    ignoreSuffixes: string[] = ['.schema', '.test'],
+): Record<string, ModelConstructor> {
+    const models: Record<string, ModelConstructor> = {};
+
+    for (const [fileName, moduleExports] of Object.entries(glob)) {
+        const name = (fileName.match(/^(.*\/)?(.+)\.(j|t)s$/) || [])[2];
+
+        if (typeof name === 'undefined') {
+            continue;
+        }
+
+        if (ignoreSuffixes.some((suffix) => name.endsWith(suffix))) {
+            continue;
+        }
+
+        const module = moduleExports['default'] ?? moduleExports[Object.keys(moduleExports)[0] as string];
+
+        if (!module || !isModelClass(module)) {
+            continue;
+        }
+
+        models[name] = module;
+    }
+
+    return models;
+}
 
 export function requireBootedModel<T extends keyof ModelsRegistry>(name: T): ModelsRegistry[T];
 export function requireBootedModel<T extends ModelConstructor<Model> = ModelConstructor<Model>>(name: string): T;
@@ -28,4 +58,13 @@ export function bootModels(
         modelClass.boot(modelName);
         bootedModels.set(modelName, modelClass as ModelConstructor);
     }
+}
+
+export function bootModelsFromViteGlob(
+    glob: Record<string, Record<string, unknown>>,
+    ignoreSuffixes: string[] = ['.schema'],
+): void {
+    const models = getModelsFromViteGlob(glob, ignoreSuffixes);
+
+    bootModels(models);
 }
