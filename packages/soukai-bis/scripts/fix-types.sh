2fa719cb72a277648d9e582fa4ec818c713409fb
Noel De Martin <noel@noeldemartin.com>
2026-02-22 10:44:45 +0100

    Fix relation types

--- packages/soukai-bis/scripts/fix-types.sh
+++ packages/soukai-bis/scripts/fix-types.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sed -i 's/}, Model>;/}, typeof Model>;/g' "dist/soukai-bis.d.ts"
-sed -i 's/typeof TypeRegistration, BelongsToManyRelation>;/typeof TypeRegistration, typeof BelongsToManyRelation>;/g' "dist/soukai-bis.d.ts"
+sed -i 's/typeof TypeRegistration, DocumentContainsManyRelation>;/typeof TypeRegistration, typeof DocumentContainsManyRelation>;/g' "dist/soukai-bis.d.ts"
