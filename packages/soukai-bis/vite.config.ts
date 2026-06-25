50a97a007fbf0e8c146c9061ce4b7a559625cdc4
Noel De Martin <noel@noeldemartin.com>
2026-06-25 19:06:28 +0200

    Disable minification and preserve modules for better debugging

--- packages/soukai-bis/vite.config.ts
+++ packages/soukai-bis/vite.config.ts
@@ -6,17 +6,22 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     build: {
         sourcemap: true,
+        minify: false,
         lib: {
             entry: {
                 'soukai-bis': fileURLToPath(new URL('./src/index.ts', import.meta.url)),
                 'patch-zod': fileURLToPath(new URL('./src/patch-zod.ts', import.meta.url)),
             },
             formats: ['es'],
-            fileName: (_, entry) => (entry.includes('patch-zod') ? 'patch-zod.js' : 'soukai-bis.js'),
         },
         rollupOptions: {
             external(source) {
                 return !source.startsWith('.') && !source.startsWith('soukai-bis/') && !source.includes('/soukai-bis/');
+            },
+            output: {
+                preserveModules: true,
+                preserveModulesRoot: 'src',
+                entryFileNames: '[name].js',
             },
         },
     },
