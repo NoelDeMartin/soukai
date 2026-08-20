c245eb6327fb12c39af3223af7e2954eb9970cc4
Noel De Martin <noel@noeldemartin.com>
2026-08-20 12:01:51 +0200

    Fix boolean fields

--- packages/soukai-bis/src/utils/rdf.ts
+++ packages/soukai-bis/src/utils/rdf.ts
@@ -33,6 +33,7 @@ export const LDP_BASIC_CONTAINER_OBJECT = new RDFNamedNode(LDP_BASIC_CONTAINER);
 export const LDP_CONTAINER_OBJECT = new RDFNamedNode(LDP_CONTAINER);
 export const LDP_CONTAINS_PREDICATE = new RDFNamedNode(LDP_CONTAINS);
 export const RDF_TYPE_PREDICATE = new RDFNamedNode(RDF_TYPE);
+export const XSD_BOOLEAN_TYPE = new RDFNamedNode(XSD_BOOLEAN);
 export const XSD_DATE_TIME_TYPE = new RDFNamedNode(XSD_DATE_TIME);
 export const XSD_INTEGER_TYPE = new RDFNamedNode(XSD_INTEGER);
 
@@ -43,6 +44,10 @@ export function createRDFLiteral(value: unknown): Literal {
 
     if (typeof value === 'number') {
         return new RDFLiteral(String(value), undefined, XSD_INTEGER_TYPE);
+    }
+
+    if (typeof value === 'boolean') {
+        return new RDFLiteral(String(value), undefined, XSD_BOOLEAN_TYPE);
     }
 
     return new RDFLiteral(String(value));
--- packages/soukai-bis/src/zod/utils.ts
+++ packages/soukai-bis/src/zod/utils.ts
@@ -1,11 +1,18 @@
 import { RDFLiteral, RDFNamedNode } from '@noeldemartin/solid-utils';
-import { ZodArray, ZodDate, ZodDefault, ZodNumber, ZodOptional, ZodURL } from 'zod';
+import { ZodArray, ZodBoolean, ZodDate, ZodDefault, ZodNumber, ZodOptional, ZodURL } from 'zod';
 import { isDevelopment, isTesting, parseDate, required } from '@noeldemartin/utils';
 import type { Quad_Object } from '@rdfjs/types';
 import type { SomeType } from 'zod/v4/core';
 
 import SoukaiError from 'soukai-bis/errors/SoukaiError';
-import { XSD_BOOLEAN, XSD_DATE_TIME, XSD_DATE_TIME_TYPE, XSD_INTEGER, XSD_INTEGER_TYPE } from 'soukai-bis/utils/rdf';
+import {
+    XSD_BOOLEAN,
+    XSD_BOOLEAN_TYPE,
+    XSD_DATE_TIME,
+    XSD_DATE_TIME_TYPE,
+    XSD_INTEGER,
+    XSD_INTEGER_TYPE,
+} from 'soukai-bis/utils/rdf';
 
 export function castToJavaScript(objects: [Quad_Object, ...Quad_Object[]], definition: SomeType): unknown {
     const finalType = getFinalType(definition);
@@ -31,6 +38,10 @@ export function castToJavaScript(objects: [Quad_Object, ...Quad_Object[]], defin
 
     if (finalType instanceof ZodNumber) {
         return Number(object.value);
+    }
+
+    if (finalType instanceof ZodBoolean) {
+        return object.value === 'true' || object.value === '1';
     }
 
     if (object.termType === 'Literal') {
@@ -68,6 +79,10 @@ export function castToRDF(value: unknown, definition: SomeType): Quad_Object[] {
 
     if (finalType instanceof ZodNumber) {
         return [new RDFLiteral(String(value), undefined, XSD_INTEGER_TYPE)];
+    }
+
+    if (finalType instanceof ZodBoolean) {
+        return [new RDFLiteral(String(value), undefined, XSD_BOOLEAN_TYPE)];
     }
 
     if (finalType instanceof ZodURL) {
